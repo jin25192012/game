b36f0944d92b768091c6e0ab161b0188f0818905
jin25192012 <jin25192012@gmail.com>
2025-10-01 21:58:26 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -22,6 +22,7 @@ self.addEventListener('install', event => {
 });
 
 // 서비스 워커 활성화 이벤트: 이전 버전의 캐시를 정리할 수 있습니다. (지금은 비워둠)
+navigator.serviceWorker.register('./sw.js')
 self.addEventListener('activate', event => {
   console.log('Service worker activating...');
 });
@@ -38,4 +39,5 @@ self.addEventListener('fetch', event => {
       })
   );
 });
+
 
